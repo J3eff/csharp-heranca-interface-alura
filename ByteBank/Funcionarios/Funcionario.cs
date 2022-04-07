3a5e2b875cc9e54504eb8867b85db9bb58d32cb3
J3eff <jeffersonvideo125@gmail.com>
2022-04-06 23:56:05 -0300

    Atalhos no VisualStudio e classe Funcionario

---
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        // 0 - funcionário
+        // 1 - diretor
+        // 2 - designer
+        // N - ....
+        private int _tipo;
+
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ByteBank.Funcionarios;
+using System;
 
 namespace ByteBank
 {
@@ -10,6 +7,16 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.897.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine("Bonificação: "  + carlos.GetBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
